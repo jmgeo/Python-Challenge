aa20d2cdaba196b6dea39e80c80aea0322e5348f
Mills Jr <j.mills.geo@gmail.com>
2021-05-08 01:16:21 -0500

    trying to deal with the poorly worded increase and decrease elements

--- PyBank/main.py
+++ PyBank/main.py
@@ -29,3 +29,29 @@ with open(csvpath) as csvfile:
         #count the total P&L
         this_month_PnL = int(row[1])
         total_PnL += this_month_PnL
+
+        if (total_months ==1):
+            #make the last month P&L equal to the value of this month's P&L
+            last_month_PnL = this_month_PnL
+            continue
+
+        else:
+            #calculate the P&L
+            PnL_change = this_month_PnL - last_month_PnL
+
+            #append to the months
+            months.append(row[0])
+
+            #append to P&L
+            PnL.append(PnL_change)
+
+            #set the current month P&L to the previous and continue the loop
+            last_month_PnL = this_month_PnL
+    
+    #calculate the sum and the average P&L over the dataset timeframe, as well as the max and min P&Ls
+    sum_PnL = sum(PnL)
+    ave_PnL = round(sum_PnL/(total_months -1),2)
+    max_change_PnL = max(PnL)
+    min_change_PnL = min(PnL)
+
+    
